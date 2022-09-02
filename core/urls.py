64d31555b9c477221c2e11700ba48c7858d5966f
istpdeveloper <istpdeveloper@gmail.com>
2022-09-02 19:53:39 +0900

    feat: sms 전송, sms 인증 로직 추가

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from core.views.sms import SMSCodeCheckView, SMSCodeRequestView
+
+
+app_name = "core"
+urlpatterns = [
+    path("sms", SMSCodeRequestView.as_view(), name="sms 요청"),
+    path("sms/verify", SMSCodeCheckView.as_view(), name="sms 검사"),
+]
